c4c5c4377fe55cf02895ee59405691d8e7a0ff68
Sskingfisher <sskingfisher123@gmail.com>
2024-06-05 03:39:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.w3c.dom.ls.LSOutput;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class A
+{
+    public void meth()
+    {
+        System.out.println("Invoking method from class A");
+    }
+}
+
+
+
+
+public class B extends A {
+
+    public void meth()
+    {
+        System.out.println("Method is overridden in Extended class B");
+    }
+    public static void main(String[] args) {
+        A a = new B();
+        a.meth();
+        a.meth();
+
     }
 }
